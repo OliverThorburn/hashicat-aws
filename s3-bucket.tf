725acb4afb53e1a72c57eacd1692797f8554748c
Oliver Thorburn <othorburn@tphglobal.com>
2021-10-13 05:32:15 +0000

    Added new S3 bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+    source = "app.terraform.io/tph/s3-bucket/aws"
+    version = "2.2.0"
+    bucket = "my-s3-bucket"
+    acl    = "private"
+    bucket_prefix = "oliverthorburn"
+
+    versioning = {
+        enabled = true
+    }
+
+}
